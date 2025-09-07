93bf0fa50ee0deac906d72baa0f1403e20ae115d
Mtsilva-code <matheuscostadasilva17@gmail.com>
2025-09-07 13:03:55 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,4 @@
+alert("Bem vindos ao jogo do amor");
+let amorDaVidaDoMatheus = "leticia";
+let chute = prompt("quem é o amor da vida do Matheus?");
+if( chute==amorDaVidaDoMatheus){ alert("Parabéns, você acertou!")} else {alert("errou:( tente novamente")};
